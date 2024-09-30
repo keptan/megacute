b13037fd0fc92a9859b143dceff04d9ca531be3f
keptan <keptan@protonmail.com>
2024-09-30 01:25:41 -0400

    report missing key

--- cpr.cpp
+++ cpr.cpp
@@ -2,14 +2,18 @@
 #include <json/json.h>
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 int main (int argc, char** argv)
 {
 
 	const std::string str = argc > 1 ? argv[1] : "verify_access_key";
+	const std::string key = getenv("HYDRUS_KEY");
+
+	if(key.size() == 0) std::cout << "set the HYDRUS_KEY enviroment variable to your hydrus API access key";
 
 	cpr::Response r = cpr::Get( cpr::Url{"http://localhost:45869/" + str},
-		cpr::Parameters{{"Hydrus-Client-API-Access-Key", "b6e34eb829c2580339f3d676d0186f19881e6db891b863c0fb74d41c204e0e3d"}
+		cpr::Parameters{{"Hydrus-Client-API-Access-Key", key}
 		});
 
 	std::cout << r.text << std::endl;
