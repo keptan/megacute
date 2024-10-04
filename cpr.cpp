73f601218a294b77867181c48e432dc4954b4348
keptan <keptan@protonmail.com>
2024-10-04 16:45:25 -0400

    working thumbnail factory

--- cpr.cpp
+++ cpr.cpp
@@ -10,54 +10,60 @@
 
 using json = nlohmann::json;
 
-class Area : public Gtk::DrawingArea
+class SearchIcon : public Glib::Object
 {
 	public:
-		Area(Glib::RefPtr<Gdk::Pixbuf> i)
-		{
-		
-			image = i;
-			set_draw_func( sigc::mem_fun(*this, &Area::on_draw));
-		}
-		virtual ~Area() = default;
 
-	protected:
-		void on_draw (const Cairo::RefPtr< Cairo::Context>& cr, int w, int h)
-		{
-			if (!image) return;
-			Gdk::Cairo::set_source_pixbuf(cr, image, 100, 100);
-			
-			cr->paint();
-		}
-		Glib::RefPtr<Gdk::Pixbuf> image;
+	const std::string fileId;
+	SearchIcon (const std::string& id) 
+		: fileId(id)
+	{
+		/*
+		auto loader = Gdk::PixbufLoader::create();
+		loader->write(reinterpret_cast<const guint8*>(data.data()), data.size());
+		loader->close();
+
+		m_pixbuf = loader->get_pixbuf();
+		*/
+	};
 };
 
-class Window : public Gtk::Window
+class SearchModel : public Gtk::GridView
 {
 	public:
-		Window (Glib::RefPtr<Gdk::Pixbuf> i)
-			: area(i)
-		{
-			set_title("megacute");
-			set_default_size(400, 400);
-			set_child(area);
-		}
-
-	protected:
-		Area area;
-		//member widget
+	SearchModel ()
+	{
+	}
 };
 
-void activated ()
+auto getThumbnail (const std::string& fid)
 {
+	std::cout << "getting thumbnail" << std::endl;
+	const std::string str =  "get_files/thumbnail";
+	const std::string key = getenv("HYDRUS_KEY");
+
+	cpr::Response r = cpr::Get( cpr::Url{"http://localhost:45869/" + str},
+		cpr::Parameters{{"Hydrus-Client-API-Access-Key", key}, 
+				{"file_id", "114169906"}
+		},
+		cpr::Header{{"Content-Type", "application/json"}});
+
+	std::string raw = r.text;
+
+	auto loader = Gdk::PixbufLoader::create();
+	loader->write(reinterpret_cast<const guint8*>(r.text.data()), r.text.size());
+	loader->close();
+
+	return loader->get_pixbuf();
 }
+
 
 int main (int argc, char** argv)
 {
 
 	auto app = Gtk::Application::create("org.gtkmm.examples.base");
 
-	const std::string str = argc > 1 ? argv[1] : "get_files/thumbnail";
+	const std::string str = argc > 1 ? argv[1] : "get_files/file";
 	const std::string key = getenv("HYDRUS_KEY");
 
 	cpr::Response r = cpr::Get( cpr::Url{"http://localhost:45869/" + str},
@@ -75,7 +81,39 @@ int main (int argc, char** argv)
 	auto builder = Gtk::Builder::create_from_file("../megacute.xml.ui");
 	auto imageWidget = builder->get_widget<Gtk::Image>("image");
 	auto window 	 = builder->get_widget<Gtk::Window>("window");
+	auto grid 	 = builder->get_widget<Gtk::GridView>("icons");
 
+	auto model   = Gio::ListStore<SearchIcon>::create();
+	auto select  = Gtk::SingleSelection::create(model);
+	auto factory = Gtk::SignalListItemFactory::create();
+	
+	factory->signal_setup().connect(
+		[&](auto l){ 
+			std::cout << "setup called" << std::endl;
+			auto picture = Gtk::make_managed<Gtk::Picture>();
+			l->set_child(*picture);
+		});
+
+	factory->signal_bind().connect(
+		[&](auto l){
+
+			std::cout << "bind called" << std::endl;
+			auto col = std::dynamic_pointer_cast<SearchIcon>(l->get_item());
+			if (!col) return;
+			auto picture = dynamic_cast<Gtk::Picture*>(l->get_child());
+			if (!picture) return;
+
+			picture->set_pixbuf( getThumbnail(col->fileId));
+		});
+
+	grid->set_model( select);
+	grid->set_factory( factory);
+
+	model->append( Glib::make_refptr_for_instance<SearchIcon>(
+			new SearchIcon("114169906")));
+
+		
+	
 	const auto activate = [&](void)
 	{
 		app->add_window(*window);
@@ -83,8 +121,9 @@ int main (int argc, char** argv)
 		imageWidget->set( loader->get_pixbuf());
 	};
 
-	app->signal_activate().connect(activate);
-	app->run(argc, argv);
 	
+
+	app->signal_activate().connect(activate);
+	return app->run(argc, argv);
 }
 
