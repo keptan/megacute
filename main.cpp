3724c7e1cfeabfdc34fe693e32c8a10e1b5e66b0
keptan <keptan@protonmail.com>
2024-10-08 22:25:47 -0400

    a main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "graphics.h"
+#include "api.h"
+
+auto main (int argc, char** argv) -> int
+{
+	Hydrus api{"http://localhost:45869/", getenv("HYDRUS_KEY")};
+	SearchWindow window(api);
+	auto future = api.search("1girl, blonde");
+	auto ids = future.get()["file_ids"].get<std::vector<FileId>>();
+
+	for(const auto i : ids) window.addFile(i);
+
+	api.setTagService("local tags");
+
+
+	return window.app->run(argc, argv);
+}
